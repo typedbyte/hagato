f469587b71d9ff52df1f14455205eb7708107166
Michael Szvetits <typedbyte@qualified.name>
2023-06-27 22:04:30 +0200

    Added documentation for hagato:with-core-effectful.

--- src/hagato-core-effectful/Hagato/Core/Effectful.hs
+++ src/hagato-core-effectful/Hagato/Core/Effectful.hs
@@ -1,3 +1,14 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Hagato.Core.Effectful
+-- Copyright   : (c) Michael Szvetits, 2023
+-- License     : BSD-3-Clause (see the file LICENSE)
+-- Maintainer  : typedbyte@qualified.name
+-- Stability   : stable
+-- Portability : portable
+--
+-- Re-export of effectful-related @Hagato.Core.*@ sub-modules.
+-----------------------------------------------------------------------------
 module Hagato.Core.Effectful
   ( module Hagato.Core
   , module Hagato.Core.Effectful.Log
--- src/hagato-core-effectful/Hagato/Core/Effectful/Log.hs
+++ src/hagato-core-effectful/Hagato/Core/Effectful/Log.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE DataKinds    #-}
 {-# LANGUAGE TypeFamilies #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Hagato.Core.Effectful.Log
+-- Copyright   : (c) Michael Szvetits, 2023
+-- License     : BSD-3-Clause (see the file LICENSE)
+-- Maintainer  : typedbyte@qualified.name
+-- Stability   : stable
+-- Portability : portable
+--
+-- A logging effect for the effectful ecosystem based on the 'Logger' type.
+-----------------------------------------------------------------------------
 module Hagato.Core.Effectful.Log
   ( Log
   , runLog
@@ -19,20 +30,24 @@ import Effectful.Dispatch.Static (SideEffects(..), StaticRep, evalStaticRep, get
 -- hagato:with-core
 import Hagato.Core.Log
 
+-- | The logging effect.
 data Log (s :: Type) :: Effect
 
 type instance DispatchOf (Log s) = Static WithSideEffects
 
 newtype instance StaticRep (Log s) = Log (Logger s)
 
+-- | Runs the logging effect.
 runLog :: IOE :> es => Logger s -> Eff (Log s : es) a -> Eff es a
 runLog = evalStaticRep . Log
 
+-- | Logs a value of type @s@.
 log :: Log s :> es => LogLevel -> s -> Eff es ()
 log level s = do
   Log (Logger f) <- getStaticRep
   unsafeEff_ $ f level s
 
+-- | An 'IO'-based version of 'log'.
 logIO :: Log s :> es => LogLevel -> IO s -> Eff es ()
 logIO level m = do
   Log (Logger f) <- getStaticRep
